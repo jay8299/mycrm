a58a70996c4afd1f972bd06d6419358948b403ef
Vijay Kumar <lathah067@gmail.com>
2020-04-08 03:25:01 +0530

    changed models

--- services/models.py
+++ services/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50,null=True)
     phone = models.CharField(max_length=10, null=True)
     email = models.CharField(max_length=50, null=True)
     profile_pic = models.ImageField(default="profile1.jpg", null=True, blank=True)
